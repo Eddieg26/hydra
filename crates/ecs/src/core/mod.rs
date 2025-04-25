e1c1a9207f5c182221fa2aea657ac497998fe64e
Eddie <eddie_0226@outlook.com>
2025-04-24 22:15:08 -0600

    refactor: moved ext to core folder

--- crates/ecs/src/core/mod.rs
+++ crates/ecs/src/core/mod.rs
@@ -1,6 +1,7 @@
 pub mod bitset;
 pub mod blob;
 pub mod dag;
+pub mod ext;
 pub mod frame;
 pub mod sparse;
 pub mod task;
@@ -8,6 +9,7 @@ pub mod task;
 pub use bitset::*;
 pub use blob::*;
 pub use dag::*;
+pub use ext::*;
 pub use frame::*;
 pub use indexmap::*;
 pub use sparse::*;
--- crates/ecs/src/lib.rs
+++ crates/ecs/src/lib.rs
@@ -1,13 +1,11 @@
 pub mod app;
 pub mod core;
-pub mod ext;
 pub mod hierarchy;
 pub mod system;
 pub mod world;
 
 pub use app::*;
 pub use core::*;
-pub use ext::*;
 pub use hierarchy::*;
 pub use system::*;
 pub use world::*;
