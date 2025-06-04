b80f3a8c970c1e41811907d0dc8981b4ebad8eec
Eddie <eddie_0226@outlook.com>
2025-06-03 20:53:35 -0600

    feat: integrate wgpu and add device and color types to render crate

---
+++ crates/render/src/device.rs
@@ -0,0 +1,30 @@
+use ecs::Resource;
+use std::sync::Arc;
+use wgpu::{Adapter, Device, Queue, RequestDeviceError};
+
+#[derive(Resource)]
+pub struct RenderDevice {
+    device: Arc<Device>,
+    pub queue: Arc<Queue>,
+}
+
+impl RenderDevice {
+    pub async fn new(adapter: &Adapter) -> Result<Self, RequestDeviceError> {
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor::default(), None)
+            .await?;
+
+        Ok(Self {
+            device: Arc::new(device),
+            queue: Arc::new(queue),
+        })
+    }
+}
+
+impl std::ops::Deref for RenderDevice {
+    type Target = Device;
+
+    fn deref(&self) -> &Self::Target {
+        &self.device
+    }
+}
---
+++ crates/render/src/types/mod.rs
@@ -0,0 +1,5 @@
+pub mod color;
+pub mod viewport;
+
+pub use color::*;
+pub use viewport::*;
