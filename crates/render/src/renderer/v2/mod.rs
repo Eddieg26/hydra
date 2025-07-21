cbf3afa1317dc9f69c79dd4b0fa957c1c9e76e2c
Eddie <eddie_0226@outlook.com>
2025-07-21 08:13:55 -0600

    chore

--- crates/render/src/renderer/v2/mod.rs
+++ crates/render/src/renderer/v2/mod.rs
@@ -1,11 +1,12 @@
 use crate::{
-    IndexBuffer, Mesh, PipelineId, RenderAssets, RenderMesh, Shader, SubMesh, VertexBuffer,
+    IndexBuffer, Mesh, MeshFormat, PipelineId, RenderAssets, RenderMesh, Shader, SubMesh,
+    VertexBuffer,
     storage::{StorageBuffer, StorageBufferArray},
 };
 use asset::{AssetId, ErasedId};
 use ecs::{Component, Entity, IndexMap};
 use encase::{ShaderType, internal::WriteInto};
-use math::{Mat4, bounds::Aabb};
+use math::{Mat4, Vec3};
 use std::collections::HashMap;
 use transform::{GlobalTransform, LocalTransform};
 use wgpu::VertexFormat;
@@ -97,17 +98,31 @@ pub struct IndirectDrawIndexedArgs {
     pub first_instance: u32,
 }
 
+#[derive(Clone, Copy, ShaderType)]
 pub struct RenderData {
     batch: u32,
     instance: u32,
     matrix: Mat4,
-    bounds: Aabb,
+    min_bounds: Vec3,
+    max_bounds: Vec3,
+    indexed: u32,
+}
+
+pub struct RenderBatch {
+    items: Vec<RenderData>,
 }
 
 pub struct DrawCall<P: RenderPhase> {
     index: usize,
     phase: P,
     pipeline: PipelineId,
+}
+
+pub struct RenderDataBuffer(StorageBufferArray<RenderData>);
+impl RenderDataBuffer {
+    pub fn write(&mut self, data: &RenderData) -> u32 {
+        todo!()
+    }
 }
 
 pub struct ModelDataBuffer<M: Model>(StorageBufferArray<M>);
@@ -136,7 +151,8 @@ pub struct ViewDrawSets<P: RenderPhase>(HashMap<Entity, Vec<DrawCall<P>>>);
 
 fn queue<D: Drawable>(
     drawables: &DrawSet<D>,
-    buffer: &mut ModelDataBuffer<D::Model>,
+    render_buffer: &mut RenderDataBuffer,
+    model_buffer: &mut ModelDataBuffer<D::Model>,
     draw_buffer: &mut DrawArgBuffer,
     draw_indexed_buffer: &mut DrawIndexedArgBuffer,
     meshes: &RenderAssets<RenderMesh>,
@@ -153,10 +169,23 @@ fn queue<D: Drawable>(
     for (batch, (key, indices)) in batches.iter().enumerate() {
         let dispatch_size = indices.len();
         for drawable in indices.iter().map(|i| &drawables.0[*i]) {
-            // Write model data into global model data buffer
-            let instance = buffer.write(&drawable.draw.model());
+            // Write model data into global cull data buffer
+            let instance = model_buffer.write(&drawable.draw.model());
             let matrix = drawable.global.matrix();
-            let bounds = *meshes.get(&key.mesh.into()).unwrap().bounds();
+            let mesh = meshes.get(&key.mesh.into()).unwrap();
+            let data = RenderData {
+                batch: batch as u32,
+                instance: instance as u32,
+                matrix,
+                min_bounds: mesh.bounds().min,
+                max_bounds: mesh.bounds().max,
+                indexed: match mesh.format() {
+                    MeshFormat::NonIndexed => 0,
+                    _ => 1,
+                },
+            };
+
+            render_buffer.write(&data);
         }
     }
 }
