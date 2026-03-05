bc6d266c421aca461c8e60782338bda24f3f0080
Eddie Goodman <eddie_0226@outlook.com>
2026-03-04 21:12:10 -0700

    feat(renderer): Add texture and sampler resource abstractions

--- crates/renderer/src/resources/mod.rs
+++ crates/renderer/src/resources/mod.rs
@@ -2,5 +2,6 @@ use std::borrow::Cow;
 
 pub mod buffer;
 pub mod id;
+pub mod texture;
 
 pub type Label = Cow<'static, str>;
---
+++ crates/renderer/src/resources/texture.rs
@@ -0,0 +1,300 @@
+use crate::{core::RenderDevice, resources::id::GpuResourceId};
+use std::{collections::HashMap, sync::Arc};
+use wgpu::{
+    CompareFunction, FilterMode, Label, SamplerBorderColor, TextureFormat, TextureUsages,
+    TextureView,
+    util::DeviceExt,
+    wgt::{TextureDataOrder, TextureDescriptor},
+};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum TextureDimension {
+    D1 {
+        size: u32,
+    },
+    D2 {
+        width: u32,
+        height: u32,
+    },
+    D2Array {
+        width: u32,
+        height: u32,
+        layers: u32,
+    },
+    D3 {
+        width: u32,
+        height: u32,
+        depth: u32,
+    },
+    Cube {
+        size: u32,
+    },
+    CubeArray {
+        size: u32,
+        layers: u32,
+    },
+}
+
+impl TextureDimension {
+    pub fn extents(&self) -> wgpu::Extent3d {
+        let (width, height, depth_or_array_layers) = match self {
+            TextureDimension::D1 { size } => (*size, 1, 1),
+            TextureDimension::D2 { width, height } => (*width, *height, 1),
+            TextureDimension::D2Array {
+                width,
+                height,
+                layers,
+            } => (*width, *height, *layers),
+            TextureDimension::D3 {
+                width,
+                height,
+                depth,
+            } => (*width, *height, *depth),
+            TextureDimension::Cube { size } => (*size, *size, 6),
+            TextureDimension::CubeArray { size, layers } => (*size, *size, *layers * 6),
+        };
+
+        wgpu::Extent3d {
+            width,
+            height,
+            depth_or_array_layers,
+        }
+    }
+}
+
+impl Into<wgpu::TextureDimension> for TextureDimension {
+    fn into(self) -> wgpu::TextureDimension {
+        match self {
+            TextureDimension::D1 { .. } => wgpu::TextureDimension::D1,
+            TextureDimension::D2 { .. } => wgpu::TextureDimension::D2,
+            TextureDimension::D2Array { .. } => wgpu::TextureDimension::D2,
+            TextureDimension::D3 { .. } => wgpu::TextureDimension::D3,
+            TextureDimension::Cube { .. } => wgpu::TextureDimension::D2,
+            TextureDimension::CubeArray { .. } => wgpu::TextureDimension::D2,
+        }
+    }
+}
+
+impl Into<wgpu::TextureViewDimension> for TextureDimension {
+    fn into(self) -> wgpu::TextureViewDimension {
+        match self {
+            TextureDimension::D1 { .. } => wgpu::TextureViewDimension::D1,
+            TextureDimension::D2 { .. } => wgpu::TextureViewDimension::D2,
+            TextureDimension::D2Array { .. } => wgpu::TextureViewDimension::D2Array,
+            TextureDimension::D3 { .. } => wgpu::TextureViewDimension::D3,
+            TextureDimension::Cube { .. } => wgpu::TextureViewDimension::Cube,
+            TextureDimension::CubeArray { .. } => wgpu::TextureViewDimension::CubeArray,
+        }
+    }
+}
+
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum WrapMode {
+    #[default]
+    ClampToEdge,
+    Repeat,
+    MirrorRepeat,
+    ClampToBorder(SamplerBorderColor),
+}
+
+impl WrapMode {
+    pub fn border_color(&self) -> Option<SamplerBorderColor> {
+        match self {
+            WrapMode::ClampToBorder(color) => Some(*color),
+            _ => None,
+        }
+    }
+}
+
+impl Into<wgpu::AddressMode> for WrapMode {
+    fn into(self) -> wgpu::AddressMode {
+        match self {
+            WrapMode::Repeat => wgpu::AddressMode::Repeat,
+            WrapMode::ClampToEdge => wgpu::AddressMode::ClampToEdge,
+            WrapMode::ClampToBorder(_) => wgpu::AddressMode::ClampToBorder,
+            WrapMode::MirrorRepeat => wgpu::AddressMode::MirrorRepeat,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum TextureSampler {
+    Default,
+    Custom {
+        filter: FilterMode,
+        wrap: WrapMode,
+        compare: Option<CompareFunction>,
+        anisotropy: u16,
+    },
+}
+
+#[derive(Clone)]
+pub struct Texture {
+    pub dimension: TextureDimension,
+    pub format: TextureFormat,
+    pub mips: bool,
+    pub usage: TextureUsages,
+    pub sampler: TextureSampler,
+    pub pixels: Vec<u8>,
+}
+
+impl Texture {
+    pub fn solid(dimension: TextureDimension) -> Self {
+        todo!()
+    }
+}
+
+pub struct GpuTexture {
+    inner: Arc<wgpu::Texture>,
+    view: TextureView,
+    sampler: SamplerId,
+}
+
+impl GpuTexture {
+    pub fn new(device: &RenderDevice, texture: &Texture, sampler: SamplerId) -> Self {
+        let mip_level_count = match texture.mips {
+            true => texture
+                .dimension
+                .extents()
+                .max_mips(texture.dimension.into()) as u32,
+            false => 1,
+        };
+
+        let texture = device.create_texture_with_data(
+            device.queue(),
+            &TextureDescriptor {
+                label: None,
+                size: texture.dimension.extents(),
+                mip_level_count,
+                sample_count: 1,
+                dimension: texture.dimension.into(),
+                format: texture.format,
+                usage: texture.usage,
+                view_formats: &[texture.format.add_srgb_suffix()],
+            },
+            TextureDataOrder::LayerMajor,
+            &texture.pixels,
+        );
+
+        let view = texture.create_view(&Default::default());
+
+        Self {
+            inner: Arc::new(texture),
+            view,
+            sampler,
+        }
+    }
+
+    pub fn inner(&self) -> &wgpu::Texture {
+        &self.inner
+    }
+
+    pub fn view(&self) -> &TextureView {
+        &self.view
+    }
+
+    pub fn sampler(&self) -> SamplerId {
+        self.sampler
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct SamplerId(usize);
+
+#[derive(Clone)]
+pub struct Sampler(wgpu::Sampler);
+
+impl std::ops::Deref for Sampler {
+    type Target = wgpu::Sampler;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub struct SamplerDesc<'a> {
+    pub label: Label<'a>,
+    pub wrap: WrapMode,
+    pub filter: FilterMode,
+    pub compare: Option<CompareFunction>,
+    pub lod_min_clamp: f32,
+    pub lod_max_clamp: f32,
+    pub anisotropy_clamp: u16,
+}
+
+impl Sampler {
+    pub fn new(device: &RenderDevice, desc: &SamplerDesc) -> Self {
+        let address_mode = desc.wrap.into();
+        let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            label: desc.label,
+            address_mode_u: address_mode,
+            address_mode_v: address_mode,
+            address_mode_w: address_mode,
+            mag_filter: desc.filter,
+            min_filter: desc.filter,
+            mipmap_filter: desc.filter,
+            lod_min_clamp: desc.lod_min_clamp,
+            lod_max_clamp: desc.lod_max_clamp,
+            compare: desc.compare,
+            anisotropy_clamp: desc.anisotropy_clamp,
+            border_color: desc.wrap.border_color(),
+        });
+
+        Self(sampler)
+    }
+}
+
+pub struct SamplerCache {
+    samplers: Vec<Sampler>,
+    allocated: HashMap<TextureSampler, SamplerId>,
+}
+
+impl SamplerCache {
+    pub fn new(default: Sampler) -> Self {
+        Self {
+            samplers: vec![default],
+            allocated: HashMap::from_iter(std::iter::once((TextureSampler::Default, SamplerId(0)))),
+        }
+    }
+
+    pub fn get(&self, id: SamplerId) -> &Sampler {
+        &self.samplers[id.0]
+    }
+
+    pub fn allocate(&mut self, device: &RenderDevice, sampler: TextureSampler) -> SamplerId {
+        if let Some(id) = self.allocated.get(&sampler) {
+            return *id;
+        } else {
+            let id = self.samplers.len();
+            let (filter, wrap, compare, anisotropy) = match sampler {
+                TextureSampler::Default => todo!(),
+                TextureSampler::Custom {
+                    filter,
+                    wrap,
+                    compare,
+                    anisotropy,
+                } => (filter, wrap, compare, anisotropy),
+            };
+
+            let desc = SamplerDesc {
+                label: None,
+                wrap,
+                filter,
+                compare,
+                anisotropy_clamp: anisotropy,
+                lod_min_clamp: 0.0,
+                lod_max_clamp: 32.0,
+            };
+
+            self.samplers.push(Sampler::new(device, &desc));
+            self.allocated.insert(sampler, SamplerId(id));
+
+            SamplerId(id)
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.samplers.clear();
+        self.allocated.clear();
+    }
+}
